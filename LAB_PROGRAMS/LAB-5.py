0a70503e3e143c87f0eb092a131ca0523e84205c
priyangapriyahh-prog <priyangapriyahh@gmail.com>
2026-07-26 11:44:12 +0530

    Add files via upload

---
+++ LAB_PROGRAMS/LAB-5.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+img = cv2.imread(r"C:\Users\Priyanga\OneDrive\Desktop\cv\Picture5.jpg")
+kernel = np.ones((5,5), np.uint8)
+erode = cv2.erode(img, kernel, iterations=1)
+cv2.imshow("Original", img)
+cv2.imshow("Eroded Image", erode)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
