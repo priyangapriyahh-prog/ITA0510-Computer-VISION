9caddb63df8752d52cfc03b134819b0f47573121
priyangapriyahh-prog <priyangapriyahh@gmail.com>
2026-07-26 14:02:20 +0530

    Add files via upload

---
+++ LAB_PROGRAMS/LAB-10.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+img = cv2.imread(r"C:\Users\Priyanga\OneDrive\Desktop\cv\Picture10.jpg")
+rows, cols = img.shape[:2]
+M = np.float32([[1, 0, 100], [0, 1, 50]])
+moved = cv2.warpAffine(img, M, (cols, rows))
+cv2.imshow("Original Image", img)
+cv2.imshow("Moved Image", moved)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
