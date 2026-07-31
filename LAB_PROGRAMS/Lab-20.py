346ec38ea652010deb81ab29fd7e860ffa223801
priyangapriyahh-prog <priyangapriyahh@gmail.com>
2026-07-31 14:30:46 +0530

    Add files via upload

---
+++ LAB_PROGRAMS/Lab-20.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+img = cv2.imread(r"C:\Users\Priyanga\OneDrive\Desktop\cv\Picture20.jpg")
+kernel = np.array([[0, -1, 0],
+                   [-1, 5, -1],
+                   [0, -1, 0]])
+sharpen = cv2.filter2D(img, -1, kernel)
+cv2.imshow("Original Image", img)
+cv2.imshow("Sharpened Image", sharpen)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
